1ddafb2becea13557e19e72f91348503cabdacff
GKolev <wiraste@gmail.com>
2015-02-07 21:20:10 +0200

    - updated gitignore file name - compressed screenshots - updated assembly info

--- Source/CustomControls/Properties/AssemblyInfo.cs
+++ Source/CustomControls/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CustomControls")]
+[assembly: AssemblyTitle("PermissionManagement")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CustomControls")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Georgi Kolev")]
+[assembly: AssemblyProduct("PermissionManagement")]
+[assembly: AssemblyCopyright("Georgi Kolev 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
